217d8056a9631fcb9fcc2f9286b949afac0ab465
Martin Kuba <mkuba@newrelic.com>
2022-05-23 15:52:21 -0700

    more renaming

--- experimental/packages/opentelemetry-api-events/src/index.ts
+++ experimental/packages/opentelemetry-api-events/src/index.ts
@@ -3,5 +3,5 @@ export * from './types/EventEmitter';
 export * from './types/EventEmitterProvider';
 export * from './types/Event';
 
-import LogsAPI from './api/events';
-export const logs = LogsAPI.getInstance();
+import EventsAPI from './api/events';
+export const events = EventsAPI.getInstance();
