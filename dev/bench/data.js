2b1388ee8c258de0c89fa3c012fc3f2d54c99670
github-action-benchmark <github@users.noreply.github.com>
2023-07-24 16:18:41 +0000

    add Benchmark (benchmarkjs) benchmark result for 218e5288c2dfb282a49e784f5efb2f1db9621c43

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689981292752,
+  "lastUpdate": 1690215521876,
   "repoUrl": "https://github.com/martinkuba/opentelemetry-js",
   "entries": {
     "Benchmark": [
@@ -34,6 +34,42 @@ window.BENCHMARK_DATA = {
             "name": "10 attributes",
             "value": 315983,
             "range": "±9.64%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Martin Kuba",
+            "username": "martinkuba",
+            "email": "martin@martinkuba.com"
+          },
+          "committer": {
+            "name": "Martin Kuba",
+            "username": "martinkuba",
+            "email": "martin@martinkuba.com"
+          },
+          "id": "218e5288c2dfb282a49e784f5efb2f1db9621c43",
+          "message": "add a simple benchmark setup",
+          "timestamp": "2023-07-21T23:10:14Z",
+          "url": "https://github.com/martinkuba/opentelemetry-js/commit/218e5288c2dfb282a49e784f5efb2f1db9621c43"
+        },
+        "date": 1690215520960,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "1 span, 1 attribute, 1 event",
+            "value": 306796,
+            "range": "±11.37%",
+            "unit": "ops/sec",
+            "extra": "10 samples"
+          },
+          {
+            "name": "10 attributes",
+            "value": 251148,
+            "range": "±12.30%",
             "unit": "ops/sec",
             "extra": "10 samples"
           }
